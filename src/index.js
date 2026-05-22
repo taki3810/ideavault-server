689e5255b688e56a3f39d6cb90fae87866215c6b
IdeaVault Developer <student@example.com>
2026-05-22 15:38:36 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ app.use(express.json());
 const client = new MongoClient(mongoUri, {
   serverApi: {
     version: ServerApiVersion.v1,
-    strict: true,
+    strict: false,
     deprecationErrors: true,
   },
 });
